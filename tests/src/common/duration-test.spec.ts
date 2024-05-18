72c018250a0be6d7cdd862d678bbfa37dbf975f5
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2024-05-18 15:10:19 +0530

    [CICD] Docs generation added by default to run on main and release/**/* branches. (#7)
    * patch: add release tag change log actions.
    
    * deps: added typedoc for generating docs.
    
    * deps: lock updated.
    
    * patch: tests added for static functions
    
    * add: script for wiki generation with separate config file
    
    * add: new action to generate docs
    
    has a wiki generator action that uses GitHub scripts to create a pull request.
    
    * patch: upgrade codecov orb version
    
    * patch: update actions version and also run command in one line
    
    * patch: runs on all the branches
    
    * patch: added explicit permissions

--- tests/src/common/duration-test.spec.ts
+++ tests/src/common/duration-test.spec.ts
@@ -5,8 +5,8 @@ import { toMilliseconds } from "../../helper";
 describe("Duration", function () {
   describe("constructor", function () {
     it("should create a new Duration instance with the correct milliseconds", function () {
-      const duration = new Duration({ days: 1, hours: 2, minutes: 30 });
-      expect(duration.toMilliseconds).toStrictEqual(toMilliseconds({ days: 1, hours: 2, minutes: 30 }));
+      const duration = new Duration({ days: 1, hours: 2, minutes: 30, milliseconds: 500, seconds: 30, weeks: 2 });
+      expect(duration.toMilliseconds).toStrictEqual(toMilliseconds({ days: 1, hours: 2, minutes: 30, milliseconds: 500, seconds: 30, weeks: 2 }));
     });
   });
 
@@ -42,6 +42,38 @@ describe("Duration", function () {
     it("should return the correct number of weeks", function () {
       const duration = new Duration({ days: 1, hours: 2, minutes: 30 });
       expect(duration.toWeeks).toStrictEqual(toMilliseconds({ days: 1, hours: 2, minutes: 30 }) / 1000 / 60 / 60 / 24 / 7);
+    });
+  });
+
+  describe("Duration static methods", () => {
+    it("should create a new Duration instance with the correct milliseconds", () => {
+      const duration = Duration.milliseconds(1000);
+      expect(duration.toMilliseconds).toBe(toMilliseconds({ milliseconds: 1000 }));
+    });
+
+    it("should create a new Duration instance with the correct seconds", () => {
+      const duration = Duration.seconds(1);
+      expect(duration.toMilliseconds).toBe(toMilliseconds({ seconds: 1 }));
+    });
+
+    it("should create a new Duration instance with the correct minutes", () => {
+      const duration = Duration.minutes(1);
+      expect(duration.toMilliseconds).toBe(toMilliseconds({ minutes: 1 }));
+    });
+
+    it("should create a new Duration instance with the correct hours", () => {
+      const duration = Duration.hours(1);
+      expect(duration.toMilliseconds).toBe(toMilliseconds({ hours: 1 }));
+    });
+
+    it("should create a new Duration instance with the correct days", () => {
+      const duration = Duration.days(1);
+      expect(duration.toMilliseconds).toBe(toMilliseconds({ days: 1 }));
+    });
+
+    it("should create a new Duration instance with the correct weeks", () => {
+      const duration = Duration.weeks(1);
+      expect(duration.toMilliseconds).toBe(toMilliseconds({ weeks: 1 }));
     });
   });
 });
